40d4d09f406e59bb7d8a3cdf4aec40b838a65320
S A B R Y E L S A W Y <164203004+Sabry-Elsawy@users.noreply.github.com>
2026-03-03 13:20:45 +0200

    build project structure

---
+++ LIfeQuest.PresentationLayer/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using LIfeQuest.PresentationLayer.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LIfeQuest.PresentationLayer.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
